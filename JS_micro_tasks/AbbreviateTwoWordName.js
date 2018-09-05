325656b2a19a89ea1d72c947436be52fe81911f7
Jakub Lajner <[jakub.lajner@gmail.com]>
2018-09-05 23:28:54 +0200

    Abbreviating a name with map and join

--- JS_micro_tasks/AbbreviateTwoWordName.js
+++ JS_micro_tasks/AbbreviateTwoWordName.js
@@ -1,4 +1,1 @@
-const abbrevName = name => name.split(' ').reduce((a,b) => `${a[0]}.${b[0]}`).toUpperCase();
-
-// Solution for more than 2 words in the name
-// const abbrevName = name => name.split(' ').reduce(((a,b) => `${a}${b[0]}.`), '').slice(0,-1).toUpperCase();
+const abbrevName = name => name.split(' ').map(item => item[0]).join('.').toUpperCase();
